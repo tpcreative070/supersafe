22b8bcf815e4021691fe6e4d75931364938f902b
TPCreative <tpcreative.co@gmail.com>
2018-09-26 21:29:52 +0700

    Delete function

---
+++ app/src/main/java/co/tpcreative/supersafe/ui/settings/AlbumSettingsActivity.java
@@ -0,0 +1,15 @@
+package co.tpcreative.supersafe.ui.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.tpcreative.supersafe.R;
+
+public class AlbumSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_settings);
+    }
+}
