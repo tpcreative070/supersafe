20c3e24fbd35ea71718f47c834127ba1624f8d53
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2020-10-28 21:56:13 +0700

    [feature-210228102020] - Converting java -> kotlin

--- checkoutlibrary/src/main/java/org/solovyev/android/checkout/PlayStoreListener.java
+++
@@ -1,8 +0,0 @@
-package org.solovyev.android.checkout;
-
-public interface PlayStoreListener {
-    /**
-     * Called when the Play Store notifies us about the purchase changes.
-     */
-    void onPurchasesChanged();
-}
