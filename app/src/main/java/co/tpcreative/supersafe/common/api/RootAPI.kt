59a6355be9a0369fc50590108a8d1f0936591170
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2020-10-31 20:37:37 +0700

    [feature-210228102020] - Fixed sync data

--- app/src/main/java/co/tpcreative/supersafe/common/api/RootAPI.kt
+++ app/src/main/java/co/tpcreative/supersafe/common/api/RootAPI.kt
@@ -16,107 +16,107 @@ import retrofit2.http.*
 
 interface RootAPI {
     @POST(TRACKING)
-    open fun onTracking(@Body request: TrackingRequest): Observable<RootResponse>?
+    fun onTracking(@Body request: TrackingRequest): Observable<RootResponse>?
 
     @POST(CHECK_VERSION)
-    open fun onCheckVersion(): Observable<BaseResponse>?
+    fun onCheckVersion(): Observable<BaseResponse>?
 
     @POST(SIGN_UP)
-    open fun onSignUP(@Body request: SignUpRequest): Observable<RootResponse>?
+    fun onSignUP(@Body request: SignUpRequest): Observable<RootResponse>?
 
     @POST(SIGN_IN)
-    open fun onSignIn(@Body request: SignInRequest): Observable<RootResponse>?
+    fun onSignIn(@Body request: SignInRequest): Observable<RootResponse>?
 
     @POST(VERIFY_CODE)
-    open fun onVerifyCode(@Body request: VerifyCodeRequest): Observable<RootResponse>?
+    fun onVerifyCode(@Body request: VerifyCodeRequest): Observable<RootResponse>?
 
     @POST(RESEND_CODE)
-    open fun onResendCode(@Body request: RequestCodeRequest): Observable<RootResponse>?
+    fun onResendCode(@Body request: RequestCodeRequest): Observable<RootResponse>?
 
     @POST(CHECK_USER_CLOUD)
-    open fun onCheckUserCloud(@Body request: UserCloudRequest): Observable<RootResponse>?
+    fun onCheckUserCloud(@Body request: UserCloudRequest): Observable<RootResponse>?
 
     @POST(ADD_USER_CLOUD)
-    open fun onAddUserCloud(@Body request: UserCloudRequest?): Observable<RootResponse>?
+    fun onAddUserCloud(@Body request: UserCloudRequest?): Observable<RootResponse>?
 
     @POST(UPDATE)
-    open fun onUpdateUser(@Body request: ChangeUserIdRequest): Observable<RootResponse>?
+    fun onUpdateUser(@Body request: ChangeUserIdRequest): Observable<RootResponse>?
 
     @POST(REFRESH_USER_TOKEN)
-    open fun onUpdateToken(@Body request: UserRequest): Observable<RootResponse>?
+    fun onUpdateToken(@Body request: UserRequest): Observable<RootResponse>?
 
     @POST(DELETE_OLD_ACCESS_TOKEN)
-    open fun onDeleteOldAccessToken(@Body request: UserRequest): Observable<RootResponse>?
+    fun onDeleteOldAccessToken(@Body request: UserRequest): Observable<RootResponse>?
 
     @POST(CHECKOUT)
-    open fun onCheckout(@Body request: CheckoutRequest): Observable<RootResponse>?
+    fun onCheckout(@Body request: CheckoutRequest): Observable<RootResponse>?
 
     @POST(CHECK_USER_ID)
-    open fun onCheckUserId(@Body request: UserRequest): Observable<RootResponse>?
+    fun onCheckUserId(@Body request: UserRequest): Observable<RootResponse>?
 
     @POST(USER_INFO)
-    open fun onUserInfo(@Body request: UserRequest): Observable<RootResponse>?
+    fun onUserInfo(@Body request: UserRequest): Observable<RootResponse>?
 
     @POST(GET_LIST_FILES_SYNC)
-    open fun onListFilesSync(@Body request: SyncItemsRequest): Observable<RootResponse>?
+    fun onListFilesSync(@Body request: SyncItemsRequest): Observable<RootResponse>?
 
     @POST(SYNC_DATA)
-    open fun onSyncData(@Body request: SyncItemsRequest): Observable<RootResponse>?
+    fun onSyncData(@Body request: SyncItemsRequest): Observable<RootResponse>?
 
     @POST(DELETE_OWN_ITEMS)
-    open fun onDeleteOwnItems(@Body request: SyncItemsRequest): Observable<RootResponse>?
+    fun onDeleteOwnItems(@Body request: SyncItemsRequest): Observable<RootResponse>?
 
     @POST(CATEGORIES_SYNC)
-    open fun onCategoriesSync(@Body request: CategoriesRequest?): Observable<RootResponse>?
+    fun onCategoriesSync(@Body request: CategoriesRequest?): Observable<RootResponse>?
 
     @POST(GET_LIST_CATEGORIES_SYNC)
-    open fun onListCategoriesSync(@Body request: CategoriesRequest?): Observable<RootResponse>?
+    fun onListCategoriesSync(@Body request: CategoriesRequest?): Observable<RootResponse>?
 
     @POST(DELETE_CATEGORIES)
-    open fun onDeleteCategories(@Body request: CategoriesRequest?): Observable<RootResponse>?
+    fun onDeleteCategories(@Body request: CategoriesRequest?): Observable<RootResponse>?
 
     @Headers("Accept: application/json")
     @GET(GET_DRIVE_ABOUT)
-    open fun onGetDriveAbout(@Header("Authorization") token: String?): Observable<DriveAbout>?
+    fun onGetDriveAbout(@Header("Authorization") token: String?): Observable<DriveAbout>?
 
     @Headers("Accept: application/json")
     @POST(SEND_MAIL)
-    open fun onSendMail(@Header("Authorization") token: String?, @Body body: EmailToken): Call<ResponseBody>?
+    fun onSendMail(@Header("Authorization") token: String?, @Body body: EmailToken): Call<ResponseBody>?
 
     @FormUrlEncoded
     @POST
-    open fun onRefreshEmailToken(@Url url: String?, @FieldMap request: MutableMap<String?, Any?>): Observable<EmailToken>?
+    fun onRefreshEmailToken(@Url url: String?, @FieldMap request: MutableMap<String?, Any?>): Observable<EmailToken>?
 
     @POST(ADD_EMAIL_TOKEN)
-    open fun onAddEmailToken(@Body request: OutlookMailRequest?): Observable<BaseResponse>?
+    fun onAddEmailToken(@Body request: OutlookMailRequest?): Observable<BaseResponse>?
 
     @Headers("Accept: application/json")
     @POST(CREATE_FOLDER)
-    open fun onCrateFolder(@Header("Authorization") token: String?, @Body request: DriveApiRequest): Observable<DriveAbout>?
+    fun onCrateFolder(@Header("Authorization") token: String?, @Body request: DriveApiRequest): Observable<DriveAbout>?
 
     @Headers("Accept: application/json")
     @GET(CHECK_IN_APP_FOLDER_EXITING)
-    open fun onCheckInAppFolderExisting(@Header("Authorization") token: String?, @Query("q") title: String?, @Query("spaces") value: String?): Observable<DriveAbout>?
+    fun onCheckInAppFolderExisting(@Header("Authorization") token: String?, @Query("q") title: String?, @Query("spaces") value: String?): Observable<DriveAbout>?
 
     @Headers("Accept: application/json")
     @GET(GET_FILES_INFO)
-    open fun onGetFilesInfo(@Header("Authorization") token: String?, @Path("id") id: String?): Observable<DriveAbout>?
+    fun onGetFilesInfo(@Header("Authorization") token: String?, @Path("id") id: String?): Observable<DriveAbout>?
 
     @Headers("Accept: application/json")
     @DELETE(DELETE_CLOUD_ITEM)
-    open fun onDeleteCloudItem(@Header("Authorization") token: String?, @Path("id") id: String?): Observable<Response<DriveAbout>>?
+    fun onDeleteCloudItem(@Header("Authorization") token: String?, @Path("id") id: String?): Observable<Response<DriveAbout>>?
 
     @Headers("Accept: application/json")
     @GET(CHECK_IN_APP_FOLDER_EXITING)
-    open fun onGetListFile(@Header("Authorization") token: String?, @Query("q") title: String?, @Query("spaces") value: String?): Observable<DriveAbout>?
+    fun onGetListFile(@Header("Authorization") token: String?, @Query("q") title: String?, @Query("spaces") value: String?): Observable<DriveAbout>?
 
     @Headers("Accept: application/json")
     @GET(GET_LIST_FILE_IN_APP_FOLDER)
-    open fun onGetListFileInAppFolder(@Header("Authorization") token: String?, @Query("spaces") value: String?): Observable<DriveAbout>?
+    fun onGetListFileInAppFolder(@Header("Authorization") token: String?, @Query("spaces") value: String?): Observable<DriveAbout>?
 
     @POST(UPLOAD_FILE_TO_GOOGLE_DRIVE)
     @Multipart
-    open fun uploadFileMultipleInAppFolder(
+    fun uploadFileMultipleInAppFolder(
             @Header("Authorization") authToken: String?,
             @Part metaPart: MultipartBody.Part?,
             @Part dataPart: MultipartBody.Part?,
@@ -124,7 +124,7 @@ interface RootAPI {
 
     @GET(DOWNLOAD_FILE_FROM_GOOGLE_DRIVE)
     @Streaming
-    open fun downloadDriveFile(@Header("Authorization") authToken: String?, @Path("id") id: String?): Observable<Response<ResponseBody>>?
+    fun downloadDriveFile(@Header("Authorization") authToken: String?, @Path("id") id: String?): Observable<Response<ResponseBody>>?
 
     companion object {
         const val ROOT_GOOGLE_DRIVE: String = "https://www.googleapis.com/"
