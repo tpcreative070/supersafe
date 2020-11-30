acf38019810d3b4fbbc121c69bba28de916b36ea
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2020-11-30 18:35:36 +0700

    [feature-113130112020] - Updated logic for update token

--- app/src/main/java/co/tpcreative/supersafe/model/User.kt
+++ app/src/main/java/co/tpcreative/supersafe/model/User.kt
@@ -6,11 +6,8 @@ class User : BaseResponse(), Serializable {
     var email: String? = null
     var _id: String? = null
     var other_email: String? = null
-    var current_milliseconds: Long = 0
     var name: String? = null
-    var role = 0
     var change = false
-    var active = false
     var code: String? = null
     var cloud_id: String? = null
     var author: Authorization? = null
@@ -18,17 +15,10 @@ class User : BaseResponse(), Serializable {
     var access_token: String? = null
     var driveConnected = false
     var driveAbout: DriveAbout? = null
-    var isRefresh = false
-    var isDownLoad = false
-    var isUpload = false
-    var isRequestSync = false
-    var isUpdateView = false
     var isWaitingSendMail = false
     var premium: Premium? = null
     var email_token: EmailToken? = null
     var syncData: SyncData? = null
-
     companion object {
-        private val instance: User? = null
     }
 }
