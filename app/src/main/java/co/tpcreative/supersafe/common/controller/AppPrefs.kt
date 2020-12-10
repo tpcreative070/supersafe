e0a37a9a8c3c4e10e13bf98f5cbc8349b5ef5409
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2020-12-10 14:55:53 +0700

    [feature-121310122020] - Added more security for master key

--- app/src/main/java/co/tpcreative/supersafe/common/controller/AppPrefs.kt
+++ app/src/main/java/co/tpcreative/supersafe/common/controller/AppPrefs.kt
@@ -2,12 +2,14 @@ package co.tpcreative.supersafe.common.controller
 import android.content.Context
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
+import co.tpcreative.supersafe.common.Navigator
+import co.tpcreative.supersafe.common.encypt.SecurityUtil
 
 object AppPrefs {
     lateinit var encryptedPrefs: Prefs
     lateinit var prefs: Prefs
     fun initEncryptedPrefs(context: Context) {
-        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS+SecurityUtil.key_password_default)
                 .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
                 .build()
         encryptedPrefs =
